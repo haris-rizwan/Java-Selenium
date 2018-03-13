9b95acb568099c2a428f8cbe2bdd2b89f6e28142
Haris Rizwan <harisrizwan@Hariss-MacBook-Pro.local>
2018-03-13 12:21:13 -0400

    bonus find all links test case

--- AutomationTutorial/src/test/tutorial/BonusFindAllLinks.java
+++ AutomationTutorial/src/test/tutorial/BonusFindAllLinks.java
@@ -8,24 +8,37 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BonusFindAllLinks {
-    WebDriver driver;
-    String baseUrl;
+   private WebDriver driver;
+   private String baseUrl;
 
     @Before
     public void setUp(){
         System.setProperty("webdriver.chrome.driver","/Users/harisrizwan/Selenium/Chrome/chromedriver");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get(baseUrl);
+        baseUrl = "https://www.expedia.com/";
+
     }
 
     @Test
     public void testFindLinks(){
+        driver.get(baseUrl);
+        List<WebElement> linkList = clickableLinks(driver);
+        for (WebElement link:linkList){
+            String url = link.getAttribute("href");
+            try {
+                System.out.println("URL "+ url +" returned " + linksStatus(new URL(url)));
+            }
+            catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
 
     }
 
@@ -47,8 +60,28 @@ public class BonusFindAllLinks {
         return linksToClick;
     }
 
+    /**
+     *
+     * @param url **requires a string of url****
+     * @return **it will return the response message when url is processed
+     */
+
     public static String linksStatus(URL url){
-        
+        try{
+            /*first create an object of HttpURlConnection and open a connection with url which passe
+            * as an argument*/
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            /*then connect */
+            http.connect();
+            /*create a string variable to store the response message*/
+            String responseMessage = http.getResponseMessage();
+            http.disconnect();
+            return responseMessage;
+
+        }
+        catch (Exception e){
+            return e.getMessage();
+        }
     }
 
     @After
